ebba1236111bac12876acc4f0a99e79c5b14a75c
Nikita Somenkov <somenkov.nikita@icloud.com>
2021-06-19 06:28:17 +0300

    Add initial version of Nachos Module template

---
+++ Nachos Templates/Nachos Module.xctemplate/___FILEBASENAME___/Presenter/___FILEBASENAME___Presenter.swift
@@ -0,0 +1,33 @@
+//___FILEHEADER___
+
+final class ___VARIABLE_productName:identifier___Presenter {
+
+    // MARK: - Public Properties
+
+    weak var view: ___VARIABLE_productName:identifier___ViewInput?
+    var router: ___VARIABLE_productName:identifier___RouterInput?
+    var output: ___VARIABLE_productName:identifier___Output?
+
+    init() {
+
+    }
+
+}
+
+// MARK: - ___VARIABLE_productName:identifier___ViewOutput
+
+extension ___VARIABLE_productName:identifier___Presenter: ___VARIABLE_productName:identifier___ViewOutput {
+
+}
+
+// MARK: - ___VARIABLE_productName:identifier___Input
+
+extension ___VARIABLE_productName:identifier___Presenter: ___VARIABLE_productName:identifier___Input {
+
+}
+
+// MARK: - Private methods
+
+private extension ___VARIABLE_productName:identifier___Presenter {
+
+}
---
+++ Nachos Templates/Nachos Module.xctemplate/___FILEBASENAME___/View/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,42 @@
+//___FILEHEADER___
+
+import UIKit
+
+final class ___VARIABLE_productName:identifier___ViewController: UIViewController {
+
+    // MARK: - Public Properties
+
+    var output: ___VARIABLE_productName:identifier___ViewOutput?
+
+    // MARK: - Subviews
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.configureAppearance()
+    }
+
+    // MARK: - Actions
+
+}
+
+// MARK: - ___VARIABLE_productName:identifier___ViewInput
+
+extension ___VARIABLE_productName:identifier___ViewController: ___VARIABLE_productName:identifier___ViewInput {
+
+}
+
+// MARK: - Private methods
+
+private extension ___VARIABLE_productName:identifier___ViewController {
+
+    func configureAppearance() {
+
+    }
+
+    func configureNavigation() {
+
+    }
+
+}

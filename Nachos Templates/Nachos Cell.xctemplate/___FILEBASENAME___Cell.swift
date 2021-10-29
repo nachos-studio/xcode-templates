62b59e2269fedcb2d31b581fc3601d923fb6a88a
Nikita Somenkov <somenkov.nikita@icloud.com>
2021-10-29 12:27:00 +0300

    Add Nachos Cell template

---
+++ Nachos Templates/Nachos Cell.xctemplate/___FILEBASENAME___Cell.swift
@@ -0,0 +1,63 @@
+//___FILEHEADER___
+
+import UIKit
+import NachosKit
+
+final class ___VARIABLE_productName:identifier___Cell: NKTableViewCell {
+
+    // MARK: - Neested Types
+
+    private enum Constant {
+
+    }
+
+    // MARK: - Public Properties
+
+    // MARK: - Subviews
+
+    // MARK: - Private Properties
+
+    // MARK: - Lifecycle
+
+    override func configureAppearance() {
+        configureView()
+        configureSubviews()
+        configureConstraints()
+        configureInitialState()
+    }
+
+    // MARK: - Actions
+
+}
+
+// MARK: - Public Methods
+
+extension ___VARIABLE_productName:identifier___Cell {
+
+    func configure(with viewModel: ___VARIABLE_productName:identifier___CellViewModel) {
+
+    }
+
+}
+
+// MARK: - Private Methods
+
+private extension ___VARIABLE_productName:identifier___Cell {
+
+    private func configureView() {
+
+    }
+
+    private func configureSubviews() {
+
+    }
+
+    private func configureConstraints() {
+
+    }
+
+    private func configureInitialState() {
+
+    }
+
+}

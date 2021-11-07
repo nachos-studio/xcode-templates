af7676082e4a493b7036ecd5a5ff434e6dffb5fc
Nikita Somenkov <somenkov.nikita@icloud.com>
2021-11-07 20:33:02 +0300

    Add checkbox about Combine based ViewModel

---
+++ Nachos Templates/Nachos View.xctemplate/GenerateViewModel/___FILEBASENAME___View.swift
@@ -0,0 +1,66 @@
+//___FILEHEADER___
+
+import UIKit
+import NachosKit
+import Combine
+
+final class ___VARIABLE_productName:identifier___View: NKView {
+
+    // MARK: - Neested Types
+
+    private enum Constant {
+
+    }
+
+    // MARK: - Public Properties
+
+    // MARK: - Subviews
+
+    // MARK: - Private Properties
+
+    private var cancellables: Set<AnyCancellable> = []
+
+    // MARK: - Lifecycle
+
+    override func configureAppearance() {
+        configureView()
+        configureSubviews()
+        configureConstraints()
+        configureInitialState()
+    }
+
+    // MARK: - Actions
+
+}
+
+// MARK: - Public Methods
+
+extension ___VARIABLE_productName:identifier___View {
+
+    func configure(viewModel: ___VARIABLE_productName:identifier___ViewModel) {
+        cancellables.removeAll()
+    }
+
+}
+
+// MARK: - Private Methods
+
+private extension ___VARIABLE_productName:identifier___View {
+
+    private func configureView() {
+
+    }
+
+    private func configureSubviews() {
+
+    }
+
+    private func configureConstraints() {
+
+    }
+
+    private func configureInitialState() {
+
+    }
+
+}

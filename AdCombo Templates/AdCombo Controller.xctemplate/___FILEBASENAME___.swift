19a65f1fb00661e90081bc95682b872f7129dc7f
Nikita Somenkov <somenkov.nikita@icloud.com>
2022-01-18 17:42:50 +0300

    Add initial AdCombo templates

---
+++ AdCombo Templates/AdCombo Controller.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,73 @@
+//___FILEHEADER___
+
+import UIKit
+
+final class ___FILEBASENAMEASIDENTIFIER___: UIViewController {
+
+    // MARK: - Public Properties
+
+    // MARK: - Subviews
+
+    private lazy var subview = UIView()
+
+    // MARK: - Private Properties
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.configureAppearance()
+    }
+
+    // MARK: - Actions
+
+    @objc func onTap() {
+
+    }
+
+}
+
+// MARK: - Public Methods
+
+extension ___FILEBASENAMEASIDENTIFIER___ {
+
+}
+
+// MARK: - Private methods
+
+private extension ___FILEBASENAMEASIDENTIFIER___ {
+
+    private func configureAppearance() {
+        configureView()
+        configureActions()
+        configureSubviews()
+        configureNavigation()
+        configureConstraints()
+        configureInitialState()
+    }
+
+    private func configureActions() {
+
+    }
+
+    private func configureNavigation() {
+
+    }
+
+    private func configureView() {
+
+    }
+
+    private func configureSubviews() {
+
+    }
+
+    private func configureConstraints() {
+
+    }
+
+    private func configureInitialState() {
+
+    }
+
+}
---
+++ AdCombo Templates/AdCombo View.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,64 @@
+//___FILEHEADER___
+
+import UIKit
+
+final class ___VARIABLE_productName:identifier___View: SFView {
+
+    // MARK: - Public Properties
+
+    var tapped: SFEmptyBlock?
+
+    // MARK: - Subviews
+
+    private lazy var titleLabel = UILabel()
+
+    // MARK: - Private Properties
+
+    // MARK: - Lifecycle
+
+    override func configureAppearance() {
+        configureView()
+        configureSubviews()
+        configureConstraints()
+        configureInitialState()
+    }
+
+    // MARK: - Actions
+
+    @objc private func onTapped() {
+        tapped?()
+    }
+
+}
+
+// MARK: - Public Methods
+
+extension ___VARIABLE_productName:identifier___View {
+
+    func set() {
+
+    }
+
+}
+
+// MARK: - Private Methods
+
+private extension ___VARIABLE_productName:identifier___View {
+
+    private func configureView() {
+
+    }
+
+    private func configureSubviews() {
+
+    }
+
+    private func configureConstraints() {
+
+    }
+
+    private func configureInitialState() {
+
+    }
+
+}
